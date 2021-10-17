25d20d54273a46ba423672bd0b97bfc730d8ebb2
dimsdputra <dimsdputra@gmail.com>
2021-10-17 18:26:59 +0700

    first commit

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/asset/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Dimas Dwi Putra</title>
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-warning shadow-sm fixed-top">
+            <div class="container">
+                <a class="navbar-brand" href="#">Dimas Dwi Putra</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#home">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#about">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#projects">Projects</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#contact">Contact Me</a>
+                    </li>
+                    </ul>
+                </div>
+            </div>
+          </nav>
+    </header>
+
+    <main>
+        <!-- jumbotron -->
+        <section>
+            <div class="jumbotron text-center mt-5" id="home">
+                <img src="asset/Group 2 1.png" alt="Dimas Dwi Putra" width="300" >
+                <h1 class="display-4">Dimas Dwi Putra</h1>
+                <p class="lead">Mahasiswa | State University of Surabaya</p>
+                <a class="navbar-brand" href="https://www.linkedin.com/in/dimas-dwi-putra-57128921b/">
+                    <img src="/asset/linkedin.png" alt="" width="30" height="30">
+                </a> |
+                <a class="navbar-brand ms-3" href="https://github.com/dimsdputra">
+                    <img src="/asset/github.png" alt="" width="30" height="30">
+                </a> | 
+                <a class="navbar-brand ms-3" href="https://www.instagram.com/_dimasdputra_/">
+                    <img src="/asset/instagram.png" alt="" width="30" height="30">
+                </a>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffffff" fill-opacity="1" d="M0,96L48,106.7C96,117,192,139,288,149.3C384,160,480,160,576,144C672,128,768,96,864,101.3C960,107,1056,149,1152,154.7C1248,160,1344,128,1392,112L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+            </div>
+        </section>
+        
+        <!--About-->
+        <section id="about">
+            <div class="container">
+                <div class="row text-center mb-3">
+                    <div class="col">
+                        <h2>About Me</h2>
+                    </div>
+                </div>
+                <div class="row justify-content-center fs-5">
+                    <div class="col-md-5">
+                        <p>Hello, introduce my name Dimas Dwi Putra. I am a student at Surabaya State University, Department of Informatics Engineering, Bachelor of Information Technology Education Study Program.</p>
+                    </div>
+                    <div class="col-md-5">
+                        <p>I have an interest in UI/UX designers, and FrontEnd developers. I'm currently exploring these two areas. I hope to continue to practice my expertise and interests in UI/UX designers and FrontEnd web developers.</p>
+                    </div>
+                </div>    
+            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffda6b" fill-opacity="1" d="M0,288L48,293.3C96,299,192,309,288,298.7C384,288,480,256,576,256C672,256,768,288,864,288C960,288,1056,256,1152,240C1248,224,1344,224,1392,224L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+        </section>
+
+        <!--Projects-->
+        <section id="projects">
+            <div class="container">
+                <div class="row text-center">
+                    <div class="col mb-4 mt-5">
+                        <h2>My Projects</h2>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4" class="a">
+                        <a href="https://dimsdputra.github.io/Twice-Landing-Page/" style="text-decoration: none;">
+                            <div class="card"> 
+                                <img src="/asset/project1.png" class="card-img-top" alt="projets">
+                                <div class="card-body">
+                                  <p class="card-text">Twice Landing Page</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffffff" fill-opacity="1" d="M0,224L48,234.7C96,245,192,267,288,250.7C384,235,480,181,576,181.3C672,181,768,235,864,245.3C960,256,1056,224,1152,218.7C1248,213,1344,235,1392,245.3L1440,256L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+        </section>
+
+        <!--Contact-->
+        <section id="contact">
+            <div class="container">
+                <div class="row text-center mb-3">
+                    <div class="col">
+                        <h2>Contact Me</h2>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="row justify-content-center ">
+                <div class="col-md-6 mb-5">
+                    <form>
+                        <div class="mb-3">
+                          <label for="name" class="form-label">Full Name</label>
+                          <input type="name" class="form-control" id="name" aria-describedby="name">
+                        </div>
+                        <div class="mb-3">
+                            <label for="Email" class="form-label">Email address</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        </div>
+                        <div class="mb-3">
+                            <label for="Pesan" class="form-label">Message</label>
+                            <textarea class="form-control" id="pesan" rows="3"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Send</button>
+                    </form>
+                </div>
+            </div>
+        </section>
+
+    </main>
+
+    <footer class="align-items-center text-white bg-warning text-center p-5 align-items-center">
+        <p style="align-items: center;">Created by <a href="https://www.linkedin.com/in/dimas-dwi-putra-57128921b/" class="text-white fw-bold" style="text-decoration: none;">Dimas Dwi Putra</a></p>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
